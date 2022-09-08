9d781734595674c4f0a1a7d7c15380b56fc4e363
Alidu Hammed Okanlawon <hoalidu@yahoo.com>
2022-09-08 14:47:18 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-/*
- * main using put() 
+/**
+ * main - using put() 
  *
  * return 0
- *
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return 0;
 

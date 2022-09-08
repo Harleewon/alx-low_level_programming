a6f9efe3f6c86a525890ef0f130923959aaf63eb
Alidu Hammed Okanlawon <hoalidu@yahoo.com>
2022-09-08 15:54:03 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print format
+ *
+ * return 0 always
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}

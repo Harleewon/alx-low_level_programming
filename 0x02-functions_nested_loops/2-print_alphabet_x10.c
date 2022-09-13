4c113aef0ad9b72b67fc210d76132528a787f63c
Alidu Hammed Okanlawon <hoalidu@yahoo.com>
2022-09-13 10:59:57 -0700

    func

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,9 @@ void print_alphabet_x10(void)
 {
 	char alpha, co;
 	co = 0;
-
 	while (co < 10)
 	{
-		for (alpha = 'a'; alpha <= 'z'; ++alpha) 
+		for (alpha = 'a'; alpha <= 'z'; ++alpha)
 		{
 			_putchar (alpha);
 		}
